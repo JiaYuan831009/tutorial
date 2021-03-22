0d4ecab4f7da97e599b070e1d0a2e33816e52989
資管所章家源 <s1810831009@gms.nutc.edu.tw>
2021-03-23 00:18:01 +0800

    connect sequelize with version 5

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,19 +1,36 @@
-class UserController {
-    getUser = (req, res) => {
-        const name = 'Jack';
-        const token = `Hi${name}`;
-        res.status(200).json({ message: 'user' })
-    }
+import models from '../models/index';
+import _ from 'lodash';
+import { QueryTypes } from 'sequelize';
 
-    postUser = (req, res) => {
-        const { body } = req;
-        const { name, phone } = body;
-        const profile = {
-            name,
-            phone
-        }
-        res.status(200).json({ profile });
-    }
+const { users } = models;
+
+class UserController {
+  getUser = async (req, res) => {
+    // const pureCommand = await models.sequelize.query('select * from users', {
+    //   type: QueryTypes.SELECT
+    // });
+    // const user = _.head(pureCommand);
+    const user = await users.findAll({
+      attributes: { exclude: ['password'] }
+    });
+    const response = _.map(user, o => {
+      return {
+        ...o.dataValues,
+        vip: true
+      };
+    });
+    res.status(200).json({ user });
+  };
+
+  postUser = async (req, res) => {
+    const { body } = req;
+    const { email, password } = body;
+    const user = await users.create({
+      email,
+      password
+    });
+    res.status(200).json({ user });
+  };
 }
 
 export default new UserController();
